0754edc7b03039e0b3f2be06b82831883eb322b7
DESKTOP-UPVT9K8\Bui Kien <buikien.dp@gmail.com>
2025-03-03 09:39:13 +0700

    Update SNR formula

--- utils/compute_SINR.m
+++ utils/compute_SINR.m
@@ -1,4 +1,4 @@
-function SINR = compute_SINR(H_comm, F_comm, F_sensing, sigmasq_ue, phiADC)
+function [SINR sumSigmaQ_ADC]= compute_SINR(H_comm, F_comm, F_sensing, sigmasq_ue, phiADC)
     [U, M_t, N_t] = size(H_comm);
     comm_signal_pow = abs(sum(sum(conj(H_comm).*F_comm, 3), 2)).^2;
     H_comm_exp = reshape(H_comm, [U, 1, M_t, N_t]);
@@ -7,11 +7,13 @@ function SINR = compute_SINR(H_comm, F_comm, F_sensing, sigmasq_ue, phiADC)
     sensing_interference = 0;
     
     %TODO
-    noiseADC = sigmasq_ue + (sigmasq_ue+comm_signal_pow).*(1-phiADC)./phiADC;
-
+    sigmaQ_ADC = (sigmasq_ue+comm_signal_pow)*(1-phiADC)*phiADC;
+    totalNoise = sigmasq_ue + sigmaQ_ADC/(phiADC^2);
+    sumSigmaQ_ADC = sum(sigmaQ_ADC);
+    
     for s = 1:size(F_sensing, 1)
         sensing_interference = sensing_interference + abs(sum(sum(conj(H_comm).*F_sensing(s, :, :), 3), 2)).^2;
     end
-    SINR= comm_signal_pow./(sum_pow - comm_signal_pow + sensing_interference + noiseADC);
+    SINR= comm_signal_pow./(sum_pow - comm_signal_pow + sensing_interference + totalNoise);
 
 end
--- utils/compute_metrics.m
+++ utils/compute_metrics.m
@@ -2,8 +2,8 @@ function [metric_struct] = compute_metrics(H_comm, F_comm, sigmasq_ue, phiADC, s
     % metric_struct.F_comm = F_comm;
     % metric_struct.F_sensing = F_sensing;
     metric_struct.power = compute_power(F_comm) + compute_power(F_sensing);
-    metric_struct.SINR = compute_SINR(H_comm, F_comm, F_sensing, sigmasq_ue, phiADC);
+    [metric_struct.SINR, sumSigmaQ_ADC] = compute_SINR(H_comm, F_comm, F_sensing, sigmasq_ue, phiADC);
     metric_struct.min_SINR = min(metric_struct.SINR);
-    metric_struct.SSNR = compute_sensing_SNR(sigmasq_radar_rcs, sensing_beamsteering, F_comm, F_sensing);
+    metric_struct.SSNR = compute_sensing_SNR(sigmasq_radar_rcs, sensing_beamsteering, F_comm, F_sensing, phiADC, sumSigmaQ_ADC);
 end
 
